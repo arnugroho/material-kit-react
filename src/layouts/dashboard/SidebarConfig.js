b4c9eb5615fd9f061e257473e14f36a439cd5b15
arnugroho <nugroho.akbar.r@gmail.com>
2022-02-27 22:51:05 +0700

    conf init

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -3,44 +3,56 @@ import Iconify from '../../components/Iconify';
 
 // ----------------------------------------------------------------------
 
-const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
+const getIcon = (name) => <Iconify icon={name} width={22} height={22}/>;
 
 const sidebarConfig = [
-  {
-    title: 'dashboard',
-    path: '/dashboard/app',
-    icon: getIcon('eva:pie-chart-2-fill')
-  },
-  {
-    title: 'user',
-    path: '/dashboard/user',
-    icon: getIcon('eva:people-fill')
-  },
-  {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: getIcon('eva:shopping-bag-fill')
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: getIcon('eva:file-text-fill')
-  },
-  {
-    title: 'login',
-    path: '/login',
-    icon: getIcon('eva:lock-fill')
-  },
-  {
-    title: 'register',
-    path: '/register',
-    icon: getIcon('eva:person-add-fill')
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: getIcon('eva:alert-triangle-fill')
-  }
+    {
+        title: 'dashboard',
+        path: '/dashboard/app',
+        icon: getIcon('eva:pie-chart-2-fill')
+    },
+    {
+        title: 'Master Data',
+        path: '/dashboard/user',
+        icon: getIcon('eva:people-fill'),
+        children: [
+            {title: 'Tipe Asset', path: '/dashboard/user'},
+            {title: 'Sistem', path: '/dashboard/user'},
+            {title: 'Sub Sistem', path: '/dashboard/user'},
+            {title: 'Kategori', path: '/dashboard/user'},
+        ]
+    },
+    {
+        title: 'user',
+        path: '/dashboard/user',
+        icon: getIcon('eva:people-fill')
+    },
+    {
+        title: 'product',
+        path: '/dashboard/products',
+        icon: getIcon('eva:shopping-bag-fill'),
+        children: [{title: 'productss', path: '/dashboard/products'}]
+    },
+    {
+        title: 'blog',
+        path: '/dashboard/blog',
+        icon: getIcon('eva:file-text-fill'),
+    },
+    {
+        title: 'login',
+        path: '/login',
+        icon: getIcon('eva:lock-fill')
+    },
+    {
+        title: 'register',
+        path: '/register',
+        icon: getIcon('eva:person-add-fill')
+    },
+    {
+        title: 'Not found',
+        path: '/404',
+        icon: getIcon('eva:alert-triangle-fill')
+    }
 ];
 
 export default sidebarConfig;
